1baf7fd8704119122dafc8f7d684cda7b0c6e594
Edward <ma.edward@hotmail.com>
2021-04-01 15:28:13 -0300

    added hover, focus, and active for links.

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,17 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-import Style from './Content.module.css';
+import Style from "./Content.module.css";
 
 const Content = () => (
   <div className={Style.Content}>
     <h1>Under Construction</h1>
     <ul>
       <li>
-        <FontAwesomeIcon icon={faLinkedinIn} size="2x" color="white" />
+        <a
+          href="https://www.linkedin.com/in/maahokgit"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedinIn}
+            size="2x"
+            color="white"
+            className={Style.fAHover}
+          />
+        </a>
       </li>
       <li>
-        <FontAwesomeIcon icon={faGithub} size="2x" color="white" />
+        <a
+          href="https://github.com/maahokgit/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="2x"
+            color="white"
+            className={Style.fAHover}
+          />
+        </a>
       </li>
     </ul>
   </div>
