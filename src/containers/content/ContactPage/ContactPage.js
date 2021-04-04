9daff3f74f6b9ac7af6262b675f68323fc0b6998
Edward <ma.edward@hotmail.com>
2021-04-04 14:07:19 -0300

    modified css and added thank you message after form submission

--- src/containers/content/ContactPage/ContactPage.js
+++ src/containers/content/ContactPage/ContactPage.js
@@ -1,6 +1,7 @@
 import emailjs from "emailjs-com";
 import { useEffect, useState } from "react";
 
+import funko_avi from "../../../assets/img/funko_avi2.png";
 import Style from "./ContactPage.module.css";
 
 const ContactPage = () => {
@@ -8,9 +9,10 @@ const ContactPage = () => {
     console.log("[useEffect] - /contact");
     document.title = "Let's Chat - Edward Ma";
   }, []);
-  
+
   const [validateEmail, setValidateEmail] = useState(false);
   const [message, setMessage] = useState("");
+  const [formSuccess, setFormSuccess] = useState(false);
 
   const validateEmailHandler = (event) => {
     const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -25,46 +27,58 @@ const ContactPage = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    console.log(e.target);
-    // emailjs
-    //   .sendForm(
-    //     process.env.REACT_APP_EMAILJI_SERVICE_ID,
-    //     process.env.REACT_APP_EMAILJI_TEMPLATE_ID,
-    //     e.target,
-    //     process.env.REACT_APP_EMAILJI_USER_ID
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJI_SERVICE_ID,
+        process.env.REACT_APP_EMAILJI_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJI_USER_ID
+      )
+      .then(
+        (result) => {
+          if (result) {
+            setFormSuccess(true);
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
+  let formInput = (
+    <>
+      <label>Name</label>
+      <input type="text" name="from_name" placeholder="Your Name" />
+      <label>Email</label>
+      <input
+        type="email"
+        name="reply_to"
+        placeholder="Your Email"
+        onChange={(e) => validateEmailHandler(e)}
+      />
+      {message !== "" ? <span>{message}</span> : null}
+      <label>Message</label>
+      <textarea name="message" placeholder="How can I help?" />
+      <button disabled={!validateEmail}>Submit</button>
+    </>
+  );
   return (
     <div className={Style.ContactPage}>
-      <h1>Let's Chat!</h1>
-      <p>
-        Fill out this contact form, and I will get back to you as soon as
-        possible.
-      </p>
-      <form className={Style.ContactForm} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="from_name" placeholder="Your Name" />
-        <label>Email</label>
-        <input
-          type="email"
-          name="reply_to"
-          placeholder="Your Email"
-          onChange={(e) => validateEmailHandler(e)}
-        />
-        {message !== "" ? <span>{message}</span> : null}
-        <label>Message</label>
-        <textarea name="message" placeholder="How can I help?" />
-        <button disabled={!validateEmail}>Submit</button>
-      </form>
+      <div className={Style.formContainer}>
+        <form className={Style.ContactForm} onSubmit={sendEmail}>
+          <h1>Let's Chat. Drop me a line!</h1>
+          {formSuccess ? (
+            <h3>
+              Message Sent! Thank you! <br /> <br />I will get back to you as soon as I see
+              it!
+            </h3>
+          ) : (
+            formInput
+          )}
+        </form>
+        <img src={funko_avi} alt="funko avi" />
+      </div>
     </div>
   );
 };
