2edfee45415ae9fab051530be39cdee688abfb73
Edward <ma.edward@hotmail.com>
2021-04-03 23:53:37 -0300

    added emailjs for contact page

--- src/containers/content/ContactPage/ContactPage.js
+++ src/containers/content/ContactPage/ContactPage.js
@@ -1,3 +1,39 @@
-const contactPage = () => <h1>Contact Page</h1>;
+import emailjs from "emailjs-com";
+
+const contactPage = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJI_SERVICE_ID,
+        process.env.REACT_APP_EMAILJI_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJI_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <h1>Contact Page</h1>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="from_name" />
+        <label>Email</label>
+        <input type="email" name="reply_to" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </>
+  );
+};
 
 export default contactPage;
