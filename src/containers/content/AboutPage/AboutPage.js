b287f90b7cb36bb376079ce302fcc3f5c87721ed
Edward <ma.edward@hotmail.com>
2021-04-06 00:37:08 -0300

    style bug fix

--- src/containers/content/AboutPage/AboutPage.js
+++ src/containers/content/AboutPage/AboutPage.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 import Style from "./AboutPage.module.css";
 
 import selfie from "../../../assets/img/selfie.jpg";
+import { Slide } from "@material-ui/core";
 
 const AboutPage = () => {
   useEffect(() => {
@@ -12,40 +13,52 @@ const AboutPage = () => {
   }, []);
   return (
     <Container className={Style.Container}>
-      <Row>
-        <Col lg={4} className={Style.Picture}>
-          <img
-            src={selfie}
-            alt="i need a better pic"
-            className={Style.selfie}
-          />
-        </Col>
-        <Col lg={8} className={Style.AboutMe}>
-          <h2>
-            My name is Edward, a web and database developer based out of
-            beautiful Nova Scotia, Canada, and the Co-Founder & CTO of{" "}
-            <Card.Link href="https://Bursity.org" target="_blank">
-              Bursity.org
-            </Card.Link>
-            .
-          </h2>
-          <p>
-            These days I spent my time expanding my knowledge of web and
-            database development or anything tech and startup related.
-          </p>
-          <p>
-            Outside of that, you will find me serving my community with{" "}
-            <Card.Link href="http://www.ns.sjaresponder.ca/" target="_blank">
-              St. John Ambulance NS/PEI
-            </Card.Link>{" "}
-            as a Volunteer First Responder and other non-profit organizations,
-            such as the{" "}
-            <Card.Link href="https://www.mentalhealthns.ca/" target="_blank">
-              Mental Health Foundation of Nova Scotia
-            </Card.Link>
-            .
-          </p>
-        </Col>
+      <Row className={Style.row}>
+        <Slide direction="down" in={true} mountOnEnter unmountOnExit>
+          <Col lg={4} className={Style.Picture}>
+            <img
+              src={selfie}
+              alt="i need a better pic"
+              className={Style.selfie}
+            />
+          </Col>
+        </Slide>
+        <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+          <Col lg={8} className={Style.AboutMe}>
+            <h2>
+              My name is Edward, a web and database developer based out of
+              beautiful Nova Scotia, Canada, and the Co-Founder & CTO of{" "}
+              <a href="https://Bursity.org" target="_blank" rel="noreferrer">
+                Bursity.org
+              </a>
+              .
+            </h2>
+            <p>
+              These days I spent my time expanding my knowledge of web and
+              database development or anything tech and startup related.
+            </p>
+            <p>
+              Outside of that, you will find me serving my community with{" "}
+              <a
+                href="http://www.ns.sjaresponder.ca/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                St. John Ambulance NS/PEI
+              </a>{" "}
+              as a Volunteer First Responder and other non-profit organizations,
+              such as the{" "}
+              <a
+                href="https://www.mentalhealthns.ca/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Mental Health Foundation of Nova Scotia
+              </a>
+              .
+            </p>
+          </Col>
+        </Slide>
       </Row>
     </Container>
   );
