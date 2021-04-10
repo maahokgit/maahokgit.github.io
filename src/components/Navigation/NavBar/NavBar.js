fd6910683c7b9f2af2f6a9190aaa53afa5a29151
Edward <ma.edward@hotmail.com>
2021-04-10 14:27:20 -0300

    styling fix

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import SideBarDrawer from "../SideBar/SideBarDrawer/SideBarDrawer";
@@ -6,15 +6,15 @@ import SideBarDrawer from "../SideBar/SideBarDrawer/SideBarDrawer";
 import Style from "./NavBar.module.css";
 
 const NavBar = (props) => (
-    <header
-      className={Style.NavBar}
-    >
+  <header className={Style.NavBar}>
+    <div>
       <Logo />
       <nav className={Style.DeskTopOnly}>
         <NavigationItems />
       </nav>
-      <SideBarDrawer clicked={props.sideBarToggleClicked}/>
-    </header>
-  );
+      <SideBarDrawer clicked={props.sideBarToggleClicked} />
+    </div>
+  </header>
+);
 
 export default NavBar;
