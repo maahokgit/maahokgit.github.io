b8733efa1bbca2f7597ed9126a20561a3b5dc45b
Edward <ma.edward@hotmail.com>
2021-04-18 19:41:07 -0300

    fixed stying contact form styling, and minor css style

--- src/containers/content/ContactPage/ContactPage.js
+++ src/containers/content/ContactPage/ContactPage.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import Slide from "@material-ui/core/Slide";
 import funko_avi from "../../../assets/img/fbavie.jpg";
 import Style from "./ContactPage.module.css";
-import { Button, TextField } from "@material-ui/core";
-import { faPaperPlane} from '@fortawesome/free-solid-svg-icons'
+import { Button, Grid, TextField } from "@material-ui/core";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Spinner from '../../../components/UI/Spinner/Spinner';
+import Spinner from "../../../components/UI/Spinner/Spinner";
 
 const ContactPage = () => {
   useEffect(() => {
@@ -16,7 +16,7 @@ const ContactPage = () => {
   const [validateEmail, setValidateEmail] = useState(false);
   const [message, setMessage] = useState("");
   const [formSuccess, setFormSuccess] = useState(false);
-  const [sending, setSending] = useState(false)
+  const [sending, setSending] = useState(false);
 
   const validateEmailHandler = (event) => {
     const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -57,7 +57,6 @@ const ContactPage = () => {
       <TextField
         id="standard-basic"
         label="Your Name"
-        placeholder="What's your name?"
         margin="normal"
         type="text"
         name="from_name"
@@ -65,38 +64,47 @@ const ContactPage = () => {
       <TextField
         id="standard-basic"
         label="Your Email"
-        placeholder="And your email?"
         margin="normal"
         type="email"
         name="from_name"
         helperText={message}
         onChange={(e) => validateEmailHandler(e)}
       />
-     <TextField
-          id="standard-multiline-static"
-          name="message"
-          label="Your Message"
-          placeholder="What's up?"
-          margin="normal"
-          multiline
-          rows={4}
-        />
-      <Button className={Style.button} type="submit" size="large" disabled={!validateEmail}
-        endIcon={<FontAwesomeIcon icon={faPaperPlane}/>}>Submit</Button> 
+      <TextField
+        id="standard-multiline-static"
+        name="message"
+        label="Your Message"
+        margin="normal"
+        multiline
+      />
+      <Button
+        className={Style.button}
+        type="submit"
+        size="large"
+        disabled={!validateEmail}
+        endIcon={<FontAwesomeIcon icon={faPaperPlane} />}
+      >
+        Submit
+      </Button>
     </>
   );
   return (
-    <div className={Style.ContactPage}>
-      <div className={Style.formContainer}>
-        <Slide direction="right" in={true} mountOnEnter unmountOnExit>
-          <form className={Style.ContactForm} onSubmit={sendEmail}>
+    <Grid container spacing={5} className={Style.formContainer}>
+      <Slide direction="right" in={true} mountOnEnter unmountOnExit>
+        <Grid item md={5} xs={12}>
+          <img src={funko_avi} alt="funko avi" />
+        </Grid>
+      </Slide>
+      <Slide direction="left" in={true} mountOnEnter unmountOnExit>
+        <Grid item md={7} xs={12}>
           <h4>
-          I’m always keeping an eye out for opportunities and collaborations!
-        </h4>
-        <h4>
-          Drop me a line if you’d like to chat.
-        </h4>
-            {sending ? <Spinner /> : formSuccess ? (
+            I’m always keeping an eye out for opportunities and collaborations!
+          </h4>
+          <h4>Drop me a line if you’d like to chat.</h4>
+          <form className={Style.ContactForm} onSubmit={sendEmail}>
+            {sending ? (
+              <Spinner />
+            ) : formSuccess ? (
               <h3>
                 Message Sent! Thank you! <br /> <br />I will get back to you as
                 soon as I see it!
@@ -105,12 +113,9 @@ const ContactPage = () => {
               formInput
             )}
           </form>
-        </Slide>
-        <Slide direction="left" in={true} mountOnEnter unmountOnExit>
-          <img src={funko_avi} alt="funko avi" style={{ maxWidth: 410 }} />
-        </Slide>
-      </div>
-    </div>
+        </Grid>
+      </Slide>
+    </Grid>
   );
 };
 
